13d0451b6718bc0dc67fd7d3a8dc4af505dcb9c6
RobertGarabetian <robertgarabetian90@gmail.com>
2024-09-30 21:37:18 -0700

    same thing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,6 @@
 
 import React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useSession, signOut } from "next-auth/react";
 
 export default function DashboardLayout({
